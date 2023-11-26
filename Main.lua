4b23830b1be8218ce1393ad15054ad2af466bc8c
reiiqz <122692712+reiiqz@users.noreply.github.com>
2023-11-27 04:06:31 +0800

    Main.lua

--- Main.lua
+++ Main.lua
@@ -124,7 +124,7 @@ local portalWorlds = {
 	hxhant_infinite = 'Ant Kingdom (Summer)',
 	opm_infinite = 'Alien Spaceship (Underwater)',
 	eclipse_portal = 'The Eclipse',
-	port_agency = 'Sky Club',
+	bsd_portal = 'Sky Club',
 }
 
 local Bonuses = {
